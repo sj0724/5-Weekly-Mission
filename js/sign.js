c47a4451519f8e97acb15d995f290c68251f4f23
박상준 <parksangjun@bagsangjun-ui-MacBookAir.local>
2024-03-15 00:08:00 +0900

    input form에 submit으로 이벤트 수정 및 로그인 함수 수정

--- js/sign.js
+++ js/sign.js
@@ -1,12 +1,9 @@
-import { passwordHidden, passwordInput, emailInput, emailCheck,data } from "./util.js";
+import { passwordHidden, passwordInput, emailInput, emailCheck,users } from "./util.js";
 
+const formElement = document.querySelector('#form__inputForm');
 const emailError = document.querySelector('.email-errorMessage');
 const passwordError = document.querySelector('.password-errorMessage');
 const eyeBtn = document.querySelector('.eyeBtn');
-const loginBtn = document.querySelector('.form__loginButton');
-
-let emailValue = '';
-let passwordValue = '';
 
 function addError(input, errormessage, message){
   input.classList.add('input--error');
@@ -34,11 +31,35 @@ function warningPassword(e){ //패스워드 input 경고 메세지
   return e.target.value ? removeError(passwordInput, passwordError) : addError(passwordInput, passwordError, '비밀번호을 입력해주세요');
 };
 
+function validateInfo({email, password, passwordConfirm}){
+  const result = true
+  if(email === ''){
+    result = false;
+    addError(emailInput, emailError, '이메일을 입력해주세요');
+  }else if(emailCheck === false){
+    result = false;
+    addError(emailInput, emailError, '올바른 이메일 주소가 아닙니다.');
+  }
+  if(password === ''){
+    result = false;
+    addError(passwordInput, passwordError, '비밀번호을 입력해주세요.')
+  }
+  if(passwordConfirm && passwordConfirm !== password){
+    result = false;
+    addError(passwordInput, passwordError, '비밀번호가 다릅니다.')
+  }
+  return result;
+}
+
 function login(e){ //로그인
   e.preventDefault();
-  emailValue = emailInput.value;
-  passwordValue = passwordInput.value;
-  const dataEmail = data.find((user) => { //이메일이 데이터에 있나 검사
+  const $email = e.target.email;
+  const $password = e.target.password.value;
+  const emailValue = $email.value;
+  const passwordValue = $password.value;
+  const validate = validateInfo({emailValue, passwordValue})
+  console.log(validate)
+  const dataEmail = users.find((user) => { //이메일이 데이터에 있나 검사
     return user.email === emailValue;
   })
   if(dataEmail){ //이메일이 데이터에 있을때
@@ -54,6 +75,6 @@ function login(e){ //로그인
 };
 
 eyeBtn.addEventListener('click', passwordHidden);
-loginBtn.addEventListener('click', login);
+formElement.addEventListener('submit', login);
 emailInput.addEventListener('focusout', warningEmail);
 passwordInput.addEventListener('focusout', warningPassword);
--- js/util.js
+++ js/util.js
@@ -3,7 +3,7 @@ export const passwordInput = document.querySelector('#password');
 export const emailInput = document.querySelector('#email');
 const emailPattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
 
-export let data = [
+export let users = [
   {name: 'test', email: 'test@codeit.com', password: 'codeit101'},
   {name: 'admin', email: 'sj07245@naver.com', password: 'sj07245'},
   {name: 'user1', email: '1234@gmail.com', password: '1234'},
@@ -18,6 +18,6 @@ export function passwordHidden(){ //password 숨김버튼
   }
 }
 
-export function emailCheck(e){ //이메일 형식 검사
-  return emailPattern.test(e);
+export function emailCheck(email){ //이메일 형식 검사
+  return emailPattern.test(email);
 };
