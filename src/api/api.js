d5a85b060bb3abdfde293cf8ed024a2915a90e69
박상준 <parksangjun@bagsangjun-ui-MacBookAir.local>
2024-04-05 16:11:07 +0900

    fetch에서 axios 변경 및 axios instance 설정

--- src/api/api.js
+++ src/api/api.js
@@ -1,32 +1,32 @@
-const url = "https://bootcamp-api.codeit.kr/api";
+import axios from "../instance/axiosInstance";
 
 export async function getSampleUser() {
-  const response = await fetch(`${url}/sample/user`);
-  const result = response.json();
+  const response = await axios.get("/sample/user");
+  const result = response.data;
   return result;
 }
 
 export async function getSampleFolder() {
-  const response = await fetch(`${url}/sample/folder`);
-  const result = response.json();
+  const response = await axios.get("/sample/folder");
+  const result = response.data;
   return result;
 }
 
 export async function getFolder(id) {
-  const response = await fetch(`${url}/users/${id}/folders`);
-  const result = response.json();
+  const response = await axios.get(`/users/${id}/folders`);
+  const result = response.data;
   return result;
 }
 
 export async function getFolderList({ folderId = "", userId }) {
   const query = `/${userId}/links?folderId=${folderId}`;
-  const response = await fetch(`${url}/users${query}`);
-  const result = response.json();
+  const response = await axios.get(`/users${query}`);
+  const result = response.data;
   return result;
 }
 
 export async function getUser(id) {
-  const response = await fetch(`${url}/users/${id}`);
-  const result = response.json();
+  const response = await axios.get(`/users/${id}`);
+  const result = response.data;
   return result;
 }
