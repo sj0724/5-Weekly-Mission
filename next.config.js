18f7fc2e4ac2bea08765fa589408d1145a6637a1
sj0724 <sj07245@naver.com>
2024-05-12 19:34:44 +0900

    fix: renderPage 커스터마이징으로 css 미적용 방지

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const execSync = require('child_process').execSync;
-
-const lastCommitCommand = 'git rev-parse HEAD';
-
 module.exports = {
-  async generateBuildId() {
-    return execSync(lastCommitCommand).toString().trim();
-  },
   compiler: {
     styledComponents: true,
   },
