445af50ddc32628319075d312804f5a6b25bc446
sj0724 <sj07245@naver.com>
2024-05-16 13:21:14 +0900

    fix: 메인 페이지 card 이미지 코드 수정

--- components/MainSectionCard/MainSectionCard.tsx
+++ components/MainSectionCard/MainSectionCard.tsx
@@ -5,36 +5,17 @@ interface SectionItem {
   title: string;
   description: string;
   id: number;
+  image: string;
 }
 
 function MainSectionCard({ item }: { item: SectionItem }) {
-  const { title, description, id } = item;
-  const [sectionImage, setSectionImage] = useState('');
-
-  useEffect(() => {
-    switch (id) {
-      case 1:
-        setSectionImage('/img_1.png');
-        break;
-      case 2:
-        setSectionImage('/img_2.png');
-        break;
-      case 3:
-        setSectionImage('/img_3.png');
-        break;
-      case 4:
-        setSectionImage('/img_4.png');
-        break;
-      default:
-        break;
-    }
-  }, [id]);
+  const { title, description, image } = item;
 
   return (
     <S.SectionCard>
       <S.TextBox__title>{title}</S.TextBox__title>
       <S.Description>{description}</S.Description>
-      <S.SectionImage src={sectionImage} alt="sectionImage" />
+      <S.SectionImage src={image} alt="sectionImage" />
     </S.SectionCard>
   );
 }
--- util/sectionDescription.ts
+++ util/sectionDescription.ts
@@ -4,21 +4,25 @@ export const sectionDescription = [
     title: '원하는 링크를 저장하세요',
     description:
       '나중에 읽고 싶은 글, 다시 보고 싶은 영상, 사고 싶은 옷, 기억하고 싶은 모든 것을 한 공간에 저장하세요.',
+    image: '/img_1.png',
   },
   {
     id: 2,
     title: '링크로 폴더를 관리하세요',
     description: '나만의 폴더를 무제한으로 만들고 다양하게 활용할 수 있습니다.',
+    image: '/img_2.png',
   },
   {
     id: 3,
     title: '저장한 링크를 공유해보세요',
     description:
       '여러 링크를 폴더에 담고 공유할 수 있습니다. 가족, 친구, 동료들에게 쉽고 빠르게 링크를 공유해 보세요.',
+    image: '/img_3.png',
   },
   {
     id: 4,
     title: '저장한 링크를 검색해보세요',
     description: '중요한 정보들을 검색으로 쉽게 찾아보세요.',
+    image: '/img_4.png',
   },
 ];
