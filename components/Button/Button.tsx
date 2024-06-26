9a3df141354d5db761075bfb440cf8d511c637d2
sj0724 <sj07245@naver.com>
2024-06-26 16:36:11 +0900

    feat: 회원가입 페이지 리액트 쿼리 적용 및 react-hook-form 리팩토링

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,10 +1,15 @@
-import { ButtonHTMLAttributes } from 'react';
+import { ButtonHTMLAttributes, useEffect } from 'react';
 import * as S from './Button.styled';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size: 'xs' | 'sm' | 'md' | 'lg';
+  isActive?: boolean;
 }
 
-export function Button({ children, size }: ButtonProps) {
-  return <S.Cta size={size}>{children}</S.Cta>;
+export function Button({ children, size, isActive }: ButtonProps) {
+  return (
+    <S.Cta size={size} disabled={isActive} isActive={isActive}>
+      {children}
+    </S.Cta>
+  );
 }
