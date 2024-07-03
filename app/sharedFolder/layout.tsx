e6312cad72437ba745ef118a82d3ae7db4893764
sj0724 <sj07245@naver.com>
2024-07-03 22:10:14 +0900

    fix: 링크 경로 수정

--- app/sharedFolder/layout.tsx
+++ app/sharedFolder/layout.tsx
@@ -12,7 +12,7 @@ export default function SharedFolderLayout({
     <div>
       <div className="sticky top-0 z-10 w-full bg-[--Background] px-[3.2rem] py-[2rem] lg:px-[20rem]">
         <div className="flex w-fit flex-col items-start justify-center">
-          <Link href="/">
+          <Link href="/folder">
             <Image src={Logo} alt="메인 로고" width={133} height={24} />
           </Link>
         </div>
